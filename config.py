66219847fddf00064bb94b201224ba659b7cdd48
KX3M <163962831+KX3M@users.noreply.github.com>
2025-05-12 02:47:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,21 +34,21 @@ VERIFY_DB = os.environ.get("VERIFY_DB", "mongodb+srv://dattebayo56:dattebayo56@a
 DBV_NAME = os.environ.get("VERIFY_DBNAME", "warBots")
 
 
-TOKEN_PIC = os.environ.get("TOKEN_PIC", "https://envs.sh/H2U.jpg")
+TOKEN_PIC = os.environ.get("TOKEN_PIC", "https://graph.org/file/f03274dfad4979c8f95d8-b0f06dbc9d0d0edb41.jpg")
 
 
 #Tutorial video for the user of your shortner on how to download.
 TUT_VID = os.environ.get("TUT_VID","https://t.me/ChipsTutorial/7")
 
 
-START_PIC = os.environ.get("START_PIC", "https://envs.sh/H2U.jpg")
-FORCE_PIC = os.environ.get("FORCE_PIC", "https://envs.sh/H2U.jpg")
+START_PIC = os.environ.get("START_PIC", "https://graph.org/file/8263af1da9ac363d8c580-a8912d879836f10ccd.jpg")
+FORCE_PIC = os.environ.get("FORCE_PIC", "https://graph.org/file/9bb59500aad7bd82a5fed-a73aed8d1313fee973.jpg")
 
                         
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #Collection of pics for Bot // #Optional but atleast one pic link should be replaced if you don't want predefined links
-PICS = (os.environ.get("PICS", "https://envs.sh/H2U.jpg")).split() #Required
+PICS = (os.environ.get("PICS", "https://graph.org/file/f03274dfad4979c8f95d8-b0f06dbc9d0d0edb41.jpg https://graph.org/file/9bb59500aad7bd82a5fed-a73aed8d1313fee973.jpg https://graph.org/file/8b18a066cbc95cbe49ca2-4a04fdb50bc94af59b.jpg https://graph.org/file/ad6982514284cbc8a8a29-5e214b4d10962fdd62.jpg https://graph.org/file/8263af1da9ac363d8c580-a8912d879836f10ccd.jpg")).split() #Required
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 
