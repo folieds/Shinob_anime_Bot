abf673d9c6093f4f989476c8f34d288ea6dccbb1
KX3M <163962831+KX3M@users.noreply.github.com>
2025-05-26 00:19:49 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -303,7 +303,7 @@ async def not_joined(client: Client, message: Message):
             pass
 
         await message.reply_photo(
-            photo=random.choice(PICS),
+            photo=FORCE_PIC,
             caption=FORCE_MSG.format(
                 first=message.from_user.first_name,
                 last=message.from_user.last_name,
